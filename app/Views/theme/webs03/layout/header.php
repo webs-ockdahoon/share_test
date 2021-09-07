9f1a66930f588c625cbe78f97fe0f72b576e4b08
bangjuyoung <webs.bangjuyoung@gmail.com>
2021-09-07 10:55:31 +0900

    문서 메타 정보 수정 및 공통 에셋 정의

--- app/Views/theme/webs03/layout/header.php
+++ app/Views/theme/webs03/layout/header.php
@@ -1,16 +1,21 @@
-<title>Webs01</title>
+<title>Dong-a University Hospital</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE-edge">
 <meta name="format-detection" content="telephone=no">
 
 <meta property="og:type" content="website" />
-<meta property="og:site_name" content="Webs001">
+<meta property="og:site_name" content="Dong-a University Hospital">
 <meta property="og:url" content="/">
-<meta property="og:title" content="Webs001">
-<meta property="og:description" content="웹스소프트, webssoft, 웹스테마, 쇼핑몰">
+<meta property="og:title" content="Dong-a University Hospital">
+<meta property="og:description" content="Dong-a University Hospital">
 <meta property="og:image" content="">
 
+<link href="/assets/plugins/swiper@4.5.3/dist/css/swiper.min.css" rel="stylesheet">
+<link href="<?php echo $THEME_URL; ?>/css/common.min.css" rel="stylesheet">
+<link href="<?php echo $THEME_URL?>/css/theme.css" rel="stylesheet">
 
-<link href="<?php echo $THEME_URL?>/css/common.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $THEME_URL?>/css/theme.css" rel="stylesheet" type="text/css" />
+<script defer src="/assets/plugins/jquery/jquery-3.6.0.min.js"></script>
+<script defer src="/assets/plugins/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<script defer src="/assets/plugins/swiper@4.5.3/dist/js/swiper.min.js"></script>
+<script defer src="<?php echo $THEME_URL; ?>/script/common.js"></script>
