a45b56f2c919858b56095a9d411a7ca2c9b25504
bangjuyoung <webs.bangjuyoung@gmail.com>
2021-09-16 17:16:48 +0900

    사용자 화면 템플릿 폴더로 분리하여 퍼블리싱 작업 계속 진행

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -8,11 +8,9 @@ class Main extends BaseController
 
 	public function index()
 	{
-        $this->setUseLayout(false); // 레이아웃은 view 에서 선택하기 위해 해당 기능 해제
 	    $this->setView("main");
 	    return $this->run();
 		//return view('welcome_message');
 	}
-
 }
 
---
+++ app/Views/theme/webs03/template/main.php
@@ -0,0 +1,5 @@
+<?php echo $this->extend($THEME_URL.'/layout/defaultLayout'); ?>
+
+<?php echo $this->section('content'); ?>
+메인 페이지 내용 입니다.
+<?php echo $this->endSection(); ?>
