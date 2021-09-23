2c8dcdacbba294acc6aaf4cb0e6ecae4e2f5a1b1
bangjuyoung <webs.bangjuyoung@gmail.com>
2021-09-23 09:33:08 +0900

    초기 파일 내용과 일치시킴

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -6,11 +6,11 @@ class Main extends BaseController
 {
     protected $viewPath = "main";
 
-	public function index()
-	{
-	    $this->setView("main");
-	    return $this->run();
-		//return view('welcome_message');
-	}
-}
+    public function index()
+    {
+        $this->setView("main");
+        return $this->run();
+        //return view('welcome_message');
+    }
 
+}
