c970d683dd9b44ee13e340fea5183f387b2f00b7
bangjuyoung <webs.bangjuyoung@gmail.com>
2021-09-29 09:31:57 +0900

    메뉴 데이터 연동 기본 작업

--- app/Views/theme/webs03/_includes/primaryNavigation.php
+++ app/Views/theme/webs03/_includes/primaryNavigation.php
@@ -1,134 +1,41 @@
 <ul class="nav primary-nav">
-    <li class="nav-item">
-        <div class="nav-link-toggler">
-            <a href="/" class="nav-link " target="_self">병원소개</a>
-            <button type="button" class="btn btn-icon link-toggler js__item-toggler" data-parent=".nav-item">
-                <span class="icon icon-toggle"></span>
-                <span class="sr-only">하위메뉴보기</span>
-            </button>
-        </div>
-        <ul class="nav-sub nav-sub0">
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">병원소개</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">감독의 인사말</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">기본 지연 목표</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">병원 이력</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">비전과 사명</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">의료 시설 데이터</a>
-            </li>
-        </ul>
-    </li>
 
-    <li class="nav-item">
-        <a href="/" class="nav-link " target="_self">부서</a>
-    </li>
+    <?php foreach($menu_info as $menu_item): ?>
 
-    <li class="nav-item">
-        <a href="/" class="nav-link " target="_self">전문센터</a>
-    </li>
+        <?php $has_sub_item = isset($menu_item['sub']) && !empty($menu_item['sub']); ?>
 
-    <li class="nav-item">
-        <div class="nav-link-toggler">
-            <a href="/" class="nav-link " target="_self">병원정보</a>
-            <button type="button" class="btn btn-icon link-toggler js__item-toggler" data-parent=".nav-item">
-                <span class="icon icon-toggle"></span>
-                <span class="sr-only">하위메뉴보기</span>
-            </button>
-        </div>
-        <ul class="nav-sub nav-sub0">
-            <li class="nav-item">
+        <li class="nav-item">
+
+            <?php if ($has_sub_item): ?>
                 <div class="nav-link-toggler">
-                    <a href="/" class="nav-link " target="_self">국제의료센터</a>
-                    <button type="button" class="btn btn-icon link-toggler js__item-toggler" data-parent=".nav-item">
-                        <span class="icon icon-toggle"></span>
-                        <span class="sr-only">하위메뉴보기</span>
-                    </button>
-                </div>
-                <ul class="nav-sub nav-sub1">
-                    <li class="nav-item">
-                        <a href="/" class="nav-link " target="_self">센터장 인사말</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link " target="_self">센터에서 일하는 직원</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link " target="_self">비즈니스 프로세스</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-item">
-                <div class="nav-link-toggler">
-                    <a href="/" class="nav-link " target="_self">위치</a>
-                    <button type="button" class="btn btn-icon link-toggler js__item-toggler" data-parent=".nav-item">
-                        <span class="icon icon-toggle"></span>
-                        <span class="sr-only">하위메뉴보기</span>
-                    </button>
-                </div>
-                <ul class="nav-sub nav-sub1">
-                    <li class="nav-item">
-                        <a href="/" class="nav-link " target="_self">본관</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link " target="_self">새 건물</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/" class="nav-link " target="_self">중앙 건물</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">컨시어지 서비스</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">정보 주차에 대해</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">소비자 서비스 기업</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">운전 경로</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">연락처</a>
-            </li>
-        </ul>
-    </li>
+            <?php endif; ?>
 
-    <li class="nav-item">
-        <div class="nav-link-toggler">
-            <a href="/" class="nav-link " target="_self">고객센터</a>
-            <button type="button" class="btn btn-icon link-toggler js__item-toggler" data-parent=".nav-item">
-                <span class="icon icon-toggle"></span>
-                <span class="sr-only">하위메뉴보기</span>
-            </button>
+                <a href="<?php echo $menu_item['mnu_url']; ?>" class="nav-link " target="_self"><?php echo $menu_item['mnu_title']; ?></a>
 
-        </div>
-        <ul class="nav-sub nav-sub0">
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">환자 문의</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">지원서</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">환자 평가</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">갤러리</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link " target="_self">병원 소식</a>
-            </li>
-        </ul>
-    </li>
+                    <?php if ($has_sub_item): ?>
+                        <button type="button" class="btn btn-icon link-toggler js__item-toggler" data-parent=".nav-item">
+                            <span class="icon icon-toggle"></span>
+                            <span class="sr-only">하위메뉴보기</span>
+                        </button>
+                    <?php endif; ?>
+
+            <?php if ($has_sub_item): ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($has_sub_item): ?>
+                <ul class="nav-sub nav-sub0">
+
+                    <?php foreach($menu_item['sub'] as $menu_sub_item): ?>
+                        <li class="nav-item">
+                            <a href="<?php echo $menu_sub_item['mnu_url']; ?>" class="nav-link " target="_self"><?php echo $menu_sub_item['mnu_title']; ?></a>
+                        </li>
+                    <?php endforeach; ?>
+
+                </ul>
+            <?php endif; ?>
+
+        </li>
+    <?php endforeach; ?>
+
 </ul>
