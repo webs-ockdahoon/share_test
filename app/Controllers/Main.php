d667425421ceed936f54b45ec0d17f4e7ee03c41
bangjuyoung <webs.bangjuyoung@gmail.com>
2021-09-29 11:51:23 +0900

    레이아웃은 View 파일에서 선택하는 방법으로 변경

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -8,6 +8,7 @@ class Main extends BaseController
 
     public function index()
     {
+        $this->setUseLayout(false); // 레이아웃은 view 에서 선택하기 위해 해당 기능 해제
         $this->setView("main");
         return $this->run();
         //return view('welcome_message');
