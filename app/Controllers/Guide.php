2a90686c740d7cc690509540e37a3e458bb2145c
bangjuyoung <webs.bangjuyoung@gmail.com>
2021-10-20 12:11:33 +0900

    편의시설 안내 페이지 생성

--- app/Controllers/Guide.php
+++ app/Controllers/Guide.php
@@ -14,6 +14,14 @@ class Guide extends BaseController
         //return view('welcome_message');
     }
 
+    public function convenience()
+    {
+        $this->setUseLayout(false); // 레이아웃은 view 에서 선택하기 위해 해당 기능 해제
+        $this->setView("convenience");
+        return $this->run();
+        //return view('welcome_message');
+    }
+
     public function location()
     {
         $this->setUseLayout(false); // 레이아웃은 view 에서 선택하기 위해 해당 기능 해제
@@ -21,5 +29,4 @@ class Guide extends BaseController
         return $this->run();
         //return view('welcome_message');
     }
-
 }
