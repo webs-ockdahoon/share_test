45c71820751401377dacd3f470a0e8ba22befa51
bangjuyoung <webs.bangjuyoung@gmail.com>
2021-10-22 17:12:35 +0900

    게시판 리스트 페이지 작업 중

--- app/Views/board/basic-with-layout01/list.php
+++ app/Views/board/basic-with-layout01/list.php
@@ -6,69 +6,95 @@
 ?>
 
 <?php echo $this->section('content'); ?>
-
-    <table class="board_list">
-        <thead>
-            <tr>
-                <th class="w100">글번호</th>
-                <th>제목</th>
-                <th class="w100">글쓴이</th>
-                <th class="w100">열람</th>
-                <th class="w100">작성일시</th>
-            </tr>
-        </thead>
-        <tbody>
-
-        <?php if ($total_count>0){
-        foreach($list as $row){
-
-            $reply = "";
-            for($k=1;$k<$row["bod_level"];$k++){
-                $reply.= "&nbsp;&nbsp;&nbsp;&nbsp;";
-            }
-            if($reply)$reply = $reply . '└&nbsp;&nbsp;';
-
-            $secret = "";
-            if($row["bod_secret"]){
-                $secret = "[비밀글] ";
-            }
-        ?>
-            <tr>
-                <td class="text-center"><?php echo $row["bod_group"]?></td>
-                <td class="text-left"><?php echo $reply . $secret?><a href="<?php echo $read_page."/".$row["bod_idx"].$qstr?>"><?php echo $row["bod_title"]?></a></td>
-                <td class="text-center"><?php echo $row["bod_writer_name"]?></td>
-                <td class="text-center"><?php echo $row["bod_read"]?></td>
-                <td class="text-center"><?php echo substr($row["bod_created_at"],0,10)?></td>
-            </tr>
-        <?php }
-        }else{?>
-            <tr>
-                <td class="text-center" colspan="5">등록된 글이 없습니다.</td>
-            </tr>
-        <?php }?>
-        </tbody>
-    </table>
-
-    <div class="board_list_foot">
-        <div class="btn_wrap"></div>
-        <div class="paging_wrap"><?php echo $links?></div>
-        <div class="btn_wrap text-right">
-            <a href="<?php echo $write_page.$qstr?>" class="btn">글작성</a>
-        </div>
-        <div class="search_wrap">
-            <form method="get">
-                <input type="hidden" name="page" value="1">
-
-                <select name="s1" id="s1">
+    <aside class="container">
+        <form method="get" role="search"  class="searchform searchform--v1">
+            <legend class="sr-only">내용 검색하기</legend>
+            <input type="hidden" name="page" value="1">
+            <div class="searchform-sidebar">
+                <select name="s1" id="s1" class="custom-select">
                     <option value="bod_title" <?php if($s1=="bod_title")echo "selected";?>>제목</option>
                     <option value="bod_content" <?php if($s1=="bod_content")echo "selected";?>>내용</option>
-                    <option value="bod_writer_name" <?php if($s1=="bod_writer_name")echo "selected";?>>글쓴이</option>
+                    <option value="bod_writer_name" <?php if($s1=="bod_writer_name")echo "selected";?>>작성자</option>
                 </select>
-                <input type="text" name="s2" id="s2" value="<?php echo $s2?>">
-                <button type="submit">검색</button>
-            </form>
+            </div>
+
+            <div class="searchform-control-submit">
+                <input type="text" name="s2" id="s2" value="<?php echo $s2?>" class="form-control" placeholder="검색어를 입력하세요.">
+                <button type="submit" class="btn btn-primary form-submit">
+                    <span class="sr-only">검색</span>
+                    <span class="material-icons-round">search</span>
+                </button>
+            </div>
+        </form>
+    </aside>
+
+    <div class="container section">
+
+        <table class="table table--v2 text-center text-muted">
+            <thead>
+                <tr>
+                <th class="d-none d-md-table-cell w-num">번호</th>
+                <th class="text-left">제목</th>
+                <th class="d-none d-md-table-cell w-author">작성자</th>
+                <th class="w-date">작성일</th>
+                <th class="d-none d-lg-table-cell w-hit">조회수</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            <tr>
+                <td class="d-none d-md-table-cell w-num">
+                    <span class="text-overline badge badge-sm badge-primary">공지</span>
+                </td>
+                <td class="text-left">
+                    <a href="" class="section-text text-dark">공지사항 제목 입니다.</a>
+                </td>
+                <td class="d-none d-md-table-cell w-author">관리자</td>
+                <td class="w-date">2021-10-22</td>
+                <td class="d-none d-lg-table-cell w-hit">1,234</td>
+            </tr>
+
+            <?php if ($total_count>0){
+
+                foreach($list as $row){
+
+                    $reply = "";
+                    for($k=1;$k<$row["bod_level"];$k++){
+                        $reply.= "&nbsp;&nbsp;&nbsp;&nbsp;";
+                    }
+                    if($reply)$reply = $reply . '└&nbsp;&nbsp;';
+
+                    $secret = "";
+                    if($row["bod_secret"]){
+                        $secret = "[비밀글] ";
+                    }
+                    ?>
+                    <tr>
+                        <td class="d-none d-md-table-cell w-num"><?php echo $row["bod_group"]?></td>
+                        <td class="text-left w-title">
+                            <?php echo $reply . $secret?>
+                            <a href="<?php echo $read_page."/".$row["bod_idx"].$qstr?>" class="section-text text-dark"><?php echo $row["bod_title"]?></a>
+                        </td>
+                        <td class="d-none d-md-table-cell w-author"><?php echo $row["bod_writer_name"]?></td>
+                        <td class="w-date"><?php echo substr($row["bod_created_at"],0,10)?></td>
+                        <td class="d-none d-lg-table-cell w-hit"><?php echo $row["bod_read"]?></td>
+                    </tr>
+                <?php }
+            }else{?>
+                <tr>
+                    <td class="empty-cell" colspan="5">등록된 글이 없습니다.</td>
+                </tr>
+            <?php }?>
+            </tbody>
+        </table>
+
+        <div class="pagination-wrapper">
+            <?php echo $links?>
+        </div>
+
+        <div>
+            <a href="<?php echo $write_page.$qstr?>" class="btn btn-primary">작성하기</a>
         </div>
     </div>
-
 <?php echo $this->endSection(); ?>
 
