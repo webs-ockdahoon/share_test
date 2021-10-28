ae668a8363c69c0c711cabadf0c3ae3f03bf53e1
bang <webs.bangjuyoung@gmail.com>
2021-10-28 11:49:36 +0900

    게시판 비밀글 확인 페이지 기본 퍼블리싱 완료

--- app/Views/board/basic-with-layout01/password.php
+++ app/Views/board/basic-with-layout01/password.php
@@ -1,24 +1,37 @@
-<div class="board_wrap">
-    <form method="post" enctype="multipart/form-data">
-        <input type="hidden" name="<?php echo $primaryKey?>" id="<?php echo $primaryKey?>" value="<?php echo $idx?>">
-        <input type="hidden" name="qstr" id="qstr" value="<?php echo $qstr;?>">
+<?php echo $this->extend($THEME_URL.'/layout/defaultSubLayout'); ?>
 
-        <div class="board_pass_check">
-            <h1>비밀번호 입력</h1>
+<?php
+    $this->setVar('bodyClassName', 'page--board page--board-'.$boc_code);
+    $this->setVar('heroTitle', $bod_title);
+    $this->setVar('heroText', $boc_title);
+?>
 
-            <div class="pass_wrap">
-                <input type="password" name="bod_password" required>
-                <br>
-                <span class="pass_help">글 작성시 입력한 비밀번호를 다시한번 입력해 주세요.</span>
+<?php echo $this->section('content'); ?>
+    <article class="container container--max-sm section">
+        <form method="post" enctype="multipart/form-data">
+            <input type="hidden" name="<?php echo $primaryKey?>" id="<?php echo $primaryKey?>" value="<?php echo $idx?>">
+            <input type="hidden" name="qstr" id="qstr" value="<?php echo $qstr;?>">
+
+            <div class="section-box section-card border shadow-sm">
+                <div class="section-divider-sm">
+                    <h3 class="section-title">비밀글 기능으로 보호하고 있습니다.</h3>
+                    <p class="section-subtitle text-secondary">글 작성시 입력한 비밀번호를 입력해 주세요.</p>
+                </div>
+
+                <div class="form-group">
+                    <label for="bod_password" class="text-dark">비밀번호</label>
+                    <input type="password" name="bod_password" id="bod_password" class="form-control" placeholder="비밀번호를 입력해 주세요." required>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-secondary btn-block btn-lg">게시물 확인하기</button>
+                </div>
             </div>
 
-        </div>
+            <div class="section-box text-center">
+                <a href="<?php echo $list_page.$qstr?>" class="btn btn-light">목록으로 돌아가기</a>
+            </div>
 
-        <div class="btn_wrap text-center">
-            <button type="submit" class="btn btn-primary">확인</button>
-            <a href="<?php echo $list_page.$qstr?>" class="btn">취소</a>
-        </div>
-
-    </form>
-
-</div>
+        </form>
+    </article>
+<?php echo $this->endSection(); ?>
