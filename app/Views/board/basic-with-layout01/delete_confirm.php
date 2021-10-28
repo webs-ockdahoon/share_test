8fe8f40ee519091590bba03693816b01b9585684
bang <webs.bangjuyoung@gmail.com>
2021-10-28 12:23:43 +0900

    게시판 게시글 삭제 확인 페이지 기본 퍼블리싱 완료

--- app/Views/board/basic-with-layout01/delete_confirm.php
+++ app/Views/board/basic-with-layout01/delete_confirm.php
@@ -1,31 +1,37 @@
-<div class="board_wrap">
-    <form method="post" enctype="multipart/form-data">
-        <input type="hidden" name="<?php echo $primaryKey?>" id="<?php echo $primaryKey?>" value="<?php echo $idx?>">
-        <input type="hidden" name="qstr" id="qstr" value="<?php echo $qstr;?>">
+<?php echo $this->extend($THEME_URL.'/layout/defaultSubLayout'); ?>
 
-        <div class="board_pass_check">
-            <h1>해당 글을 삭제하시겠습니까?</h1>
+<?php
+    $this->setVar('bodyClassName', 'page--board page--board-delete');
+$this->setVar('heroTitle', '글 삭제하기');
+?>
 
-            <div class="pass_wrap">
+<?php echo $this->section('content'); ?>
+    <div class="container container--max-sm">
+        <form method="post" enctype="multipart/form-data">
+            <input type="hidden" name="<?php echo $primaryKey?>" id="<?php echo $primaryKey?>" value="<?php echo $idx?>">
+            <input type="hidden" name="qstr" id="qstr" value="<?php echo $qstr;?>">
 
-                글 삭제 후 절대 복구할 수 없습니다.<br>
-                해당 글을 삭제하시겠습니까?
+            <div class="section-box section-card border">
+                <div class="section-divider-sm">
+                    <h3 class="section-title">글 삭제 후 절대 복구할 수 없습니다.</h3>
+                    <p class="section-subtitle text-secondary">해당 글을 정말 삭제하시겠습니까?</p>
+                </div>
 
                 <?php if(!$auth_check){?>
-                <br><br>
-                <input type="password" name="bod_password" required>
-                <br>
-                <span class="pass_help">글 작성시 입력한 비밀번호를 다시한번 입력해 주세요.</span>
+                    <div class="form-group">
+                        <label for="bod_password">비밀번호</label>
+                        <input type="password" name="bod_password" id="bod_password" class="form-control" required>
+                        <p class="form-text text-muted">글 작성시 입력한 비밀번호를 입력해 주세요.</p>
+                    </div>
                 <?php }?>
+
+                <button type="submit" class="btn btn-danger btn-block">삭제하기</button>
             </div>
 
-        </div>
+            <div class="section-box section-card pt-0 text-center">
+                <a href="<?php echo $list_page.$qstr?>" class="btn btn-light">목록으로 돌아가기</a>
+            </div>
+        </form>
 
-        <div class="btn_wrap text-center">
-            <button type="submit" class="btn btn-danger">삭제</button>
-            <a href="<?php echo $list_page.$qstr?>" class="btn">취소</a>
-        </div>
-
-    </form>
-
-</div>
+    </div>
+<?php echo $this->endSection(); ?>
