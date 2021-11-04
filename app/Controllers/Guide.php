e78b236a3fe92022320ffdfa3439c57e2b28f498
bang <webs.bangjuyoung@gmail.com>
2021-11-04 10:36:22 +0900

    이용 안내 컨트롤러에 층별 안내 view 파일 설정

--- app/Controllers/Guide.php
+++ app/Controllers/Guide.php
@@ -14,6 +14,14 @@ class Guide extends BaseController
         //return view('welcome_message');
     }
 
+    public function floor()
+    {
+        $this->setUseLayout(false); // 레이아웃은 view 에서 선택하기 위해 해당 기능 해제
+        $this->setView("floor");
+        return $this->run();
+        //return view('welcome_message');
+    }
+
     public function convenience()
     {
         $this->setUseLayout(false); // 레이아웃은 view 에서 선택하기 위해 해당 기능 해제
