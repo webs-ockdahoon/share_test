39420b1f51b4083e554d827775c645f2cd9464e6
webs-leeyejin <webs.leeyejin@gmail.com>
2021-11-26 16:54:32 +0900

    [한] 병원소식 - 진료후기 작업

--- app/Controllers/Mrequest.php
+++ app/Controllers/Mrequest.php
@@ -13,4 +13,12 @@ class Mrequest extends BaseController
         return $this->run();
         //return view('welcome_message');
     }
+
+    public function reviewInquiry()
+    {
+        $this->setUseLayout(false); // 레이아웃은 view 에서 선택하기 위해 해당 기능 해제
+        $this->setView("reviewInquiry");
+        return $this->run();
+        //return view('welcome_message');
+    }
 }
